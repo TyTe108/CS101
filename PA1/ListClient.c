70ce4e7f51dbecb4a65837dcf17935fad337b439
TyTe108 <tylim@ucsc.edu>
2019-10-11 02:51:04 -0700

    Update ListClient.c

--- PA1/ListClient.c
+++ PA1/ListClient.c
@@ -14,53 +14,47 @@ int main(int argc, char* argv[]){
    List C = NULL;
    int i;
 
-   printf("1 \n");
    for(i=1; i<=20; i++){
       append(A,i);
-      printf("2 \n");
       prepend(B,i);
-      printf("3 \n");
    }
-   printf("4 \n");
+
+   printList(stdout,A); 
+   printf("\n");
+   printList(stdout,B); 
+   printf("\n");
+
+   for(moveFront(A); index(A)>=0; moveNext(A)){
+      printf("%d ", get(A));
+   }
+   printf("\n");
+   for(moveBack(B); index(B)>=0; movePrev(B)){
+      printf("%d ", get(B));
+   }
+   printf("\n");
+
+   C = copyList(A);
+   printf("%s\n", equals(A,B)?"true":"false");
+   printf("%s\n", equals(B,C)?"true":"false");
+   printf("%s\n", equals(C,A)?"true":"false");
+
+
+   moveFront(A);
+   for(i=0; i<5; i++) moveNext(A); // at index 5
+   insertBefore(A, -1);            // at index 6
+   for(i=0; i<9; i++) moveNext(A); // at index 15
+   insertAfter(A, -2);
+   for(i=0; i<5; i++) movePrev(A); // at index 10
+   delete(A);
    printList(stdout,A);
    printf("\n");
-   printList(stdout,B);
-   printf("\n");
-   printf("5 \n");
+   printf("%d\n", length(A));
+   clear(A);
+   printf("%d\n", length(A));
 
-   
-   
-   /* for(moveFront(A); index(A)>=0; moveNext(A)){ */
-   /*    printf("%d ", get(A)); */
-   /* } */
-   /* printf("\n"); */
-   /* for(moveBack(B); index(B)>=0; movePrev(B)){ */
-   /*    printf("%d ", get(B)); */
-   /* } */
-   /* printf("\n"); */
-
-   /* C = copyList(A); */
-   /* printf("%s\n", equals(A,B)?"true":"false"); */
-   /* printf("%s\n", equals(B,C)?"true":"false"); */
-   /* printf("%s\n", equals(C,A)?"true":"false"); */
-
-
-   /* moveFront(A); */
-   /* for(i=0; i<5; i++) moveNext(A); // at index 5 */
-   /* insertBefore(A, -1);            // at index 6 */
-   /* for(i=0; i<9; i++) moveNext(A); // at index 15 */
-   /* insertAfter(A, -2); */
-   /* for(i=0; i<5; i++) movePrev(A); // at index 10 */
-   /* delete(A); */
-   /* printList(stdout,A); */
-   /* printf("\n"); */
-   /* printf("%d\n", length(A)); */
-   /* clear(A); */
-   /* printf("%d\n", length(A)); */
-
-   /* freeList(&A); */
-   /* freeList(&B); */
-   /* freeList(&C); */
+   freeList(&A);
+   freeList(&B);
+   freeList(&C);
 
    return(0);
 }
