9f3a7799230b41ed9bf6c0e6cdaac9ae1302e839
TyTe108 <tylim@ucsc.edu>
2019-10-12 19:02:05 -0700

    Update TestSort.c

--- PA1/TestSort.c
+++ PA1/TestSort.c
@@ -20,6 +20,37 @@ int main(){
 
   printf(arrayOfString[1]);
   printf("\n");
+  
+  
+  List indicesList = newList(); //indices list constructed
+   for (int i = 0; i < count; i++){ //make i 0 again to reuse, now iterate through
+	   if (length(indicesList) == 0){
+		//if indicesList is an empty list, just append the first index into it, no need to compare
+		   append(indicesList,i);
+	   }else{
+		   moveFront(indicesList); //move to head to start comparint each one
+		   while(index(indicesList)<length(indicesList)){
+			   int result = strcmp(arrayOfString[get(indicesList)], arrayOfString[i]);
+			   if (result >= 0){
+				   insertBefore(indicesList,i);
+				   moveBack(indicesList);
+			   }
+			   else{
+				   int ind = index(indicesList);
+				   if (ind == length(indicesList)-1){ //if tail, just insertAfter the tail
+					   append(indicesList,i);
+					   moveBack(indicesList);	   
+				   }
+				   else{
+					   moveNext(indicesList); //go to the next one
+				   }
+			   }
+		   }
+	   }
+   }
+		 
+   printList(stdout, indicesList);
+   freeList(&indicesList);
  
   return(0);
 }
