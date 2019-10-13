fdde54cdcc9fb28bd87d23f2cb9fe6c661ef6ef0
TyTe108 <tylim@ucsc.edu>
2019-10-12 21:24:56 -0700

    Update Lex.c

--- PA1/Lex.c
+++ PA1/Lex.c
@@ -76,14 +76,14 @@ int main(int argc, char * argv[]){
       }
       	
 	strcpy(arrayOfString[i],tokenlist);
-	printf("The arrayOfString are: \n");
-	printf(arrayOfString[i]);
+	//printf("The arrayOfString are: \n");
+	//printf(arrayOfString[i]);
 	//printf("\n");
 	i++;
 	
-      fprintf(out, "line %d contains %d token%s: \n", count, n, n==1?"":"s");
-      fprintf(out, "%s\n", tokenlist);
-      fprintf(out, "count: %d \n", count);
+//       fprintf(out, "line %d contains %d token%s: \n", count, n, n==1?"":"s");
+//       fprintf(out, "%s\n", tokenlist);
+//       fprintf(out, "count: %d \n", count);
 
    }
 
@@ -97,49 +97,49 @@ int main(int argc, char * argv[]){
 	   if (length(indicesList) == 0){
 		//if indicesList is an empty list, just append the first index into it, no need to compare
 		   append(indicesList,i);
-		   printf("#1\n");
+// 		   printf("#1\n");
 	   }else{
 		   moveFront(indicesList); //move to head to start comparint each one
-		   printf("#2\n");
+// 		   printf("#2\n");
 		   int repeat = 1;
 		   while(repeat == 1){
-			   printf("Went into while loop\n");
+// 			   printf("Went into while loop\n");
 			   int result = strcmp(arrayOfString[get(indicesList)], arrayOfString[i]);
-			   printf("#3\n");
+// 			   printf("#3\n");
 			   if (result >= 0){
-				   printf("#4\n");
+// 				   printf("#4\n");
 				   insertBefore(indicesList,i);
-				   printf("#5\n");
+// 				   printf("#5\n");
 				   //moveBack(indicesList);
 				   repeat =0;
-				   printf("#6\n");
-				   printf("Check Point #1: index [%d] with string of ", i);
-				   printf(arrayOfString[i]);
-				   printf("\n");
+// 				   printf("#6\n");
+// 				   printf("Check Point #1: index [%d] with string of ", i);
+// 				   printf(arrayOfString[i]);
+// 				   printf("\n");
 			   }
 			   else{
 				   int ind = index(indicesList);
 				   printf("#7\n");
 				   if (ind == length(indicesList)-1){ //if tail, just insertAfter the tail
-					   printf("#8\n");
+					   //printf("#8\n");
 					   append(indicesList,i);
-					   printf("#9\n");
+					   //printf("#9\n");
 					   //moveBack(indicesList);
 					   repeat = 0;
-					   printf("#10\n");
-					   printf("Check Point #2: index [%d] with string of ", i);
-					   printf(arrayOfString[i]);
-					   printf("\n");
+					   //printf("#10\n");
+					   //printf("Check Point #2: index [%d] with string of ", i);
+					   //printf(arrayOfString[i]);
+					   //printf("\n");
 					   
 				   }
 				   else{
-					   printf("Check Point #3: index [%d] with string of ", i);
-					   printf(arrayOfString[i]);
-					   printf("\n");
+					   //printf("Check Point #3: index [%d] with string of ", i);
+					   //printf(arrayOfString[i]);
+					   //printf("\n");
 					   repeat = 1;
-					   printf("#11\n");
+					   //printf("#11\n");
 					   moveNext(indicesList); //go to the next one
-					   printf("#12\n");
+					   //printf("#12\n");
 				   }
 			   }
 		   }
