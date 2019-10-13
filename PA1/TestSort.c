694b395e3f18259514e767c1274508297c1fc358
TyTe108 <tylim@ucsc.edu>
2019-10-12 19:48:17 -0700

    Update TestSort.c

--- PA1/TestSort.c
+++ PA1/TestSort.c
@@ -8,15 +8,15 @@ int main(){
   int count = 5;
   char arrayOfString[count][MAX_LEN];
 
-  strcpy(arrayOfString [1], "one");
+  strcpy(arrayOfString [0], "one");
 
-  strcpy(arrayOfString [2], "two");
+  strcpy(arrayOfString [1], "two");
 
-  strcpy(arrayOfString [3], "three");
+  strcpy(arrayOfString [2], "three");
 
-  strcpy(arrayOfString [4], "four");
+  strcpy(arrayOfString [3], "four");
 
-  strcpy(arrayOfString [5], "five");
+  strcpy(arrayOfString [4], "five");
 
   printf(arrayOfString[1]);
   printf("\n");
