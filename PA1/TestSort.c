7897c06395e06f6307585a372977baeb21b6f037
TyTe108 <tylim@ucsc.edu>
2019-10-12 18:32:04 -0700

    Create TestSort.c

---
+++ PA1/TestSort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "List.h"
+
+int main(){
+
+int count = 5;
+char arrayOfString =[count][MAX_LEN];
+
+arrayOfString [1] = "One";
+
+arrayOfString= [2] = "Two"
+
+arrayOfString [3] = "Four"
+
+arrayOfString [4] = "Five"
+
+arrayOfString [5] = "Six"
+
+printf(arrayOfString[1]);
+
+
+}
