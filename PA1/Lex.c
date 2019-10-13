2c7220e89144e2e84f72940707d036acfebcf820
TyTe108 <tylim@ucsc.edu>
2019-10-12 19:00:54 -0700

    Update Lex.c

--- PA1/Lex.c
+++ PA1/Lex.c
@@ -70,7 +70,7 @@ int main(int argc, char * argv[]){
       while( token!=NULL ){
          strcat(tokenlist, "   ");
          strcat(tokenlist, token);
-         strcat(tokenlist, "\n");
+         //strcat(tokenlist, "\n");
          n++;
          token = strtok(NULL, " \n");
       }
