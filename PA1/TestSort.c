8fb284884f3b37cc5198c32fe810152c04f2e536
TyTe108 <tylim@ucsc.edu>
2019-10-12 20:46:07 -0700

    Update TestSort.c

--- PA1/TestSort.c
+++ PA1/TestSort.c
@@ -69,7 +69,7 @@ int main(){
 					   printf("#12\n");
 				   }
 			   }
-		   }while(index(indicesList)<(length(indicesList)));
+		   }while(index(indicesList)<(length(indicesList)-1));
 	   }
    }
 		 
