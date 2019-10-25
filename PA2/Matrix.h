181ee72ae7d36feb080726e9c597c0077d5fbb9a
TyTe108 <tylim@ucsc.edu>
2019-10-24 22:42:05 -0700

    Update Matrix.h

--- PA2/Matrix.h
+++ PA2/Matrix.h
@@ -1,4 +1,14 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <stdio.h>
+#include "List.h"
+
+typedef struct EntryObj* Entry;
+typedef struct MatrixObj* Matrix;
+
 struct MatrixObj{
     List* a;
 };
-typedef struct MatrixObj* Matrix;
+
+#endif //MATRIX_H
