386e97a018e8fe727e9bb1d257a1d9b1887d7d73
TyTe108 <tylim@ucsc.edu>
2019-11-11 15:46:51 -0800

    Update ListTest.c

--- PA3/ListTest.c
+++ PA3/ListTest.c
@@ -14,7 +14,8 @@ int main(int argc, char* argv[]){
   List B = newList();
   List C = newList();
   long X[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-  int i, u=-1, v=-2, equal=0;
+  int i, equal=0;
+  long u=-1, v=-2;
 
    // initialize Lists A and B
    for(i=1; i<=20; i++){
