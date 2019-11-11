1add98b1c7608860bea969453365189bed96204b
TyTe108 <tylim@ucsc.edu>
2019-11-11 14:55:48 -0800

    Update ListTest.c

--- PA3/ListTest.c
+++ PA3/ListTest.c
@@ -18,8 +18,8 @@ int main(int argc, char* argv[]){
 
    // initialize Lists A and B
    for(i=1; i<=20; i++){
-      append(A, &X[i]);
-      prepend(B, &X[i]);
+      append(A, X[i]);
+      prepend(B, X[i]);
    }
 
    // print both lists of integers in forward direction
@@ -32,61 +32,61 @@ int main(int argc, char* argv[]){
    }
    printf("\n");
 
-   // print both lists of integers in backward direction
-   for(moveBack(A); index(A)>=0; movePrev(A)){
-      printf("%ld ", *(long*)get(A));
-   }
-   printf("\n");
-   for(moveBack(B); index(B)>=0; movePrev(B)){
-      printf("%ld ", *(long*)get(B));
-   }
-   printf("\n");
+//    // print both lists of integers in backward direction
+//    for(moveBack(A); index(A)>=0; movePrev(A)){
+//       printf("%ld ", *(long*)get(A));
+//    }
+//    printf("\n");
+//    for(moveBack(B); index(B)>=0; movePrev(B)){
+//       printf("%ld ", *(long*)get(B));
+//    }
+//    printf("\n");
 
-   // make a shallow copy of A
-   moveFront(A);
-   while( index(A)>=0 ){
-      append(C, get(A));
-      moveNext(A);
-   }
-   // print the copy in forward direction
-   for(moveFront(C); index(C)>=0; moveNext(C)){
-      printf("%ld ", *(long*)get(C));
-   }
-   printf("\n");
+//    // make a shallow copy of A
+//    moveFront(A);
+//    while( index(A)>=0 ){
+//       append(C, get(A));
+//       moveNext(A);
+//    }
+//    // print the copy in forward direction
+//    for(moveFront(C); index(C)>=0; moveNext(C)){
+//       printf("%ld ", *(long*)get(C));
+//    }
+//    printf("\n");
 
-   // check shallow equality of A and C by comparing pointers
-   equal = (length(A)==length(C));
-   moveFront(A);
-   moveFront(C);
-   while( index(A)>=0 && equal){
-      equal = ( get(A)==get(C) );
-      moveNext(A);
-      moveNext(C);
-   }
-   printf("A equals C is %s\n", (equal?"true":"false") );
+//    // check shallow equality of A and C by comparing pointers
+//    equal = (length(A)==length(C));
+//    moveFront(A);
+//    moveFront(C);
+//    while( index(A)>=0 && equal){
+//       equal = ( get(A)==get(C) );
+//       moveNext(A);
+//       moveNext(C);
+//    }
+//    printf("A equals C is %s\n", (equal?"true":"false") );
 
-   moveFront(A);
-   for(i=0; i<5; i++) moveNext(A); // at index 5
-   insertBefore(A, &u);            // now at index 6
-   for(i=0; i<9; i++) moveNext(A); // at index 15
-   insertAfter(A, &v);             // doesn't change index
-   for(i=0; i<5; i++) movePrev(A); // at index 10
-   delete(A);                      // index is now undefined
+//    moveFront(A);
+//    for(i=0; i<5; i++) moveNext(A); // at index 5
+//    insertBefore(A, &u);            // now at index 6
+//    for(i=0; i<9; i++) moveNext(A); // at index 15
+//    insertAfter(A, &v);             // doesn't change index
+//    for(i=0; i<5; i++) movePrev(A); // at index 10
+//    delete(A);                      // index is now undefined
 
-   // print A in forward and backward direction
-   for(moveFront(A); index(A)>=0; moveNext(A)){
-      printf("%ld ", *(long*)get(A));
-   }
-   printf("\n");
-   for(moveBack(A); index(A)>=0; movePrev(A)){
-      printf("%ld ", *(long*)get(A));
-   }
-   printf("\n");
+//    // print A in forward and backward direction
+//    for(moveFront(A); index(A)>=0; moveNext(A)){
+//       printf("%ld ", *(long*)get(A));
+//    }
+//    printf("\n");
+//    for(moveBack(A); index(A)>=0; movePrev(A)){
+//       printf("%ld ", *(long*)get(A));
+//    }
+//    printf("\n");
 
-   // check length of A, before and after clear()
-   printf("%d\n", length(A));
-   clear(A);
-   printf("%d\n", length(A));
+//    // check length of A, before and after clear()
+//    printf("%d\n", length(A));
+//    clear(A);
+//    printf("%d\n", length(A));
 
    //printf("Crashed before freeList \n");
    freeList(&A);
