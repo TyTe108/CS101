c4358654e442fbbf9d42ba45b25434f5ab8bdbbd
TyTe108 <tylim@ucsc.edu>
2019-11-13 15:24:18 -0800

    Update BigIntegerTest.c

--- PA3/BigIntegerTest.c
+++ PA3/BigIntegerTest.c
@@ -12,16 +12,18 @@ int main(int argc, char* argv[]){
 
     BigInteger A = newBigInteger();
     if (sign(A) != 0){
-        printf("Sign Test Failed \n");
+        printf("Sign Test Failed 1 \n");
     }
 
             BigInteger C = stringToBigInteger("+13378008135");
-//             BigInteger D = stringToBigInteger("-13378008135");
-//             if(sign(C) != 1 || sign(D) != -1) return 1;
+            BigInteger D = stringToBigInteger("-13378008135");
+            if(sign(C) != 1 || sign(D) != -1){
+                printf("Sign Test Failed 2 \n");
+            }
 
-//             freeBigInteger(&A);
-//             freeBigInteger(&C);
-//             freeBigInteger(&D);
+            freeBigInteger(&A);
+            freeBigInteger(&C);
+            freeBigInteger(&D);
 
 return(0);
 }
