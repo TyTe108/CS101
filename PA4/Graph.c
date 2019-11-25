34c622aad84295a9c30b832da569e9071087293a
TyTe108 <tylim@ucsc.edu>
2019-11-24 23:38:41 -0800

    Update Graph.c

--- PA4/Graph.c
+++ PA4/Graph.c
@@ -13,5 +13,48 @@
 
 Graph newGraph(int n){
     Graph G = (struct GraphObj *) malloc(sizeof(struct GraphObj));
+    G->vertices = n;
+    
+    G->a = M->_a = malloc((n+1) *  sizeof(List));  //Dynamic Memory #2; //An array of Lists whose ith element contains the neighbors of vertex i
+    int i = 0;
+    for (i = 1; i<=n; i++){
+        (G->a)[i] = newList(); //same as *(a+i) //Dynamic Memory #3
+    }
+ 
+    G->color = malloc((n+1) * sizeof(int));
+    G->parents = malloc((n+1) * sizeof(int));
+    G->source = 0;
+    G->distFromSource = malloc((n+1) * sizeof(int));
+    
+    return G;
+}
 
+void freeGraph(Graph* pG){
+    Graph M = *pG;
+    
+    if (G == NULL){
+    return;
+  }
+    int n = G->vertices;
+    for (int i = 1; i<=n; i++){ 
+        List freeThis = G->a[i];
+        freeList(&freeThis); 
+    }
+    free(G->a);
+    G->a= NULL;
+    
+    free(G->color);
+    G->color = NULL;
+    
+    free(G->parents);
+    G->parents = NULL;
+    
+    free(G->distFromSource);
+    G->distFromSource = NULL;
+    
+    free(*pG );
+    *pG = NULL;
+
+    
+    
 }
