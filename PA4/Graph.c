a9040aed5cba1a06ce3df13c9429d1283b81107b
TyTe108 <tylim@ucsc.edu>
2019-11-24 20:22:08 -0800

    Create Graph.c

---
+++ PA4/Graph.c
@@ -0,0 +1,16 @@
+/*==========
+ *Tyler Te
+ *CruzID: tylim
+ *Assignment: PA4
+ *========== */
+ 
+
+#include<string.h>
+#include <stdlib.h>
+#include<stdio.h> 
+#include "List.h"
+
+Graph newGraph(int n){
+
+
+}
