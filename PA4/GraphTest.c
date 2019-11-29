f625c75c53946e9b5ff765ea815de86009addd02
TyTe108 <tylim@ucsc.edu>
2019-11-29 04:12:54 -0800

    Update GraphTest.c

--- PA4/GraphTest.c
+++ PA4/GraphTest.c
@@ -16,8 +16,17 @@ int main(int argc, char* argv[]){
  Graph A = newGraph(100);
  
  if (getSize(A) != 0){
-     printf("getSize() test failed. \n"); 
+     printf("getSize() #1 test failed. \n"); 
  }
- 
+    addArc(A, 54, 1);    
+    addArc(A, 54, 2);  
+    addArc(A, 54, 3);      
+    addArc(A, 1, 54);
+    addArc(A, 1, 55);
+    if (getSize(A) != 5){
+        printf("getSize() #2 test failed because of faulty addArc \n");
+    }
+    
+    printf("getSize() test passed \n");
  return(0);
 }
